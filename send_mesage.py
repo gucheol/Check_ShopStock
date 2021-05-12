1ff66978baa357c7c518a7faa8c2f8bf9b70fba2
gucheol <9fe.kwon@apptest.ai>
2021-05-12 19:31:15 +0900

    .

---
+++ send_mesage.py
@@ -0,0 +1,19 @@
+import telegram
+import os
+
+def read_log():
+    log_path = os.path.abspath(os.path.join(__file__, '..', 'test.log'))
+    f = open(log_path, 'r', encoding='utf8')
+    text = f.read()
+    f.close()
+    return text
+
+def send_message_to_telegram(text):
+    HTTP_token = "1807456494:AAEc8mICKrJCQT8Xp2Ioo51aB6PPNtkI9-c"
+    chat_id_number = "1274502069"
+    bot = telegram.Bot(token=HTTP_token)
+    bot.sendMessage(chat_id=chat_id_number, text=text)
+
+if __name__ == '__main__':
+    text = read_log() # 저장한 가맹점재고 결과 로그 불러오기
+    send_message_to_telegram(text) # 텔레그램으로 메시지 보내기
